bbdf5b74d193719f8de39ebcb3a11cc1c8ed9660
Lucas Pinheiro <lucaspinheiroalberdanha@gmail.com>
2021-10-10 11:49:17 -0300

    deploy commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 import './App.css';
 
 function App() {
   return (
-    <Switch>
-      <Route
-        path="/carteira"
-        render={ () => (
-          <Wallet />
-        ) }
-      />
-      <Route
-        exact
-        path="/"
-        render={ () => (
-          <Login />
-        ) }
-      />
-    </Switch>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Switch>
+        <Route
+          path="/carteira"
+          render={ () => (
+            <Wallet />
+          ) }
+        />
+        <Route
+          exact
+          path="/"
+          render={ () => (
+            <Login />
+          ) }
+        />
+      </Switch>
+      </Router>
   );
 }
 
