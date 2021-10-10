25df2b1342f811bc389aaf65ad7e5a109c55dfb8
Lucas Pinheiro <lucaspinheiroalberdanha@gmail.com>
2021-10-10 11:43:34 -0300

    Updates

---
+++ precache-manifest.facc1e7353e36e54f84e5292106a2173.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12b3a53432d2f67ff1962d9fedf5686a",
+    "url": "/trybewallet/index.html"
+  },
+  {
+    "revision": "c05325c5faa730e9bb44",
+    "url": "/trybewallet/static/css/main.4830a2c2.chunk.css"
+  },
+  {
+    "revision": "f2c2f4318ac56137dfda",
+    "url": "/trybewallet/static/js/2.fe5f9b3f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybewallet/static/js/2.fe5f9b3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c05325c5faa730e9bb44",
+    "url": "/trybewallet/static/js/main.246d63bf.chunk.js"
+  },
+  {
+    "revision": "5dc72ebdcdec490a7442",
+    "url": "/trybewallet/static/js/runtime-main.ec83dc77.js"
+  },
+  {
+    "revision": "b15e3dfb5ddab89636c42c00696f37ab",
+    "url": "/trybewallet/static/media/cartoon-wallet.b15e3dfb.png"
+  }
+]);
